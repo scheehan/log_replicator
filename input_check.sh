77b1f44fa1f31e21846de4f133c091bf65fc3b5e
Han <siowcheehan@gmail.com>
2020-03-06 17:18:18 +0800

    Update input_check.sh

--- input_check.sh
+++ input_check.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Check user int input with 7 digit function
-
 function checkinput(){
 	v_in=$1
 
-	# check if input value matched interger 0-9 and 7 digit length
 	if [[ $v_in =~ ^[0-9]{1,6}$ ]] ; 
 	then
 		echo "$v_in"
